4e527d00427adf0d9afa8566742ebcc4f2f3cdbe
duong-vo <vodh2@miamioh.edu>
2023-03-11 01:25:54 -0500

    create a drop down template for the frontend

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,15 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 import Editor from "@monaco-editor/react";
 
 function CodeEditor() {
   return (
+    <div>
    <Editor
      height="90vh"
+     width = "80vh"
      defaultLanguage="javascript"
      defaultValue="// some comment"
    />
+   <Dropdown>
+        <Dropdown.Toggle variant="success">
+          Open Menu
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#">
+            Home Page
+          </Dropdown.Item>
+          <Dropdown.Item href="#">
+            Settings
+          </Dropdown.Item>
+          <Dropdown.Item href="#">
+            Logout
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+   </div>
   )
 }
 
