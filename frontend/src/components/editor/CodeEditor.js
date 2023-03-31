6bf62a5bf9fa2ca2c63777f71c56a857b7919751
duong-vo <vodh2@miamioh.edu>
2023-03-31 00:03:34 -0400

    there is a bug that the host client does not display, added dropdown and handle dropdown event

--- frontend/src/components/editor/CodeEditor.js
+++ frontend/src/components/editor/CodeEditor.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
 import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 
@@ -8,7 +9,12 @@ import Editor from "@monaco-editor/react";
 function CodeEditor() {
     const [socket, setSocket] = useState(null);
     const [name, setName] = useState(null);
+    const [language, setLanguage] = useState("javascript");
     const [userList, setUserList] = useState([]);
+
+    const handleSelect = (event) => { 
+        setLanguage(event);
+    }
 
     // use effect is similar to componentDidMount(), meaning 
     // this will trigger when the componenet is rendered
@@ -27,15 +33,15 @@ function CodeEditor() {
             return
         // test connection
         const name = prompt("Insert name");
+        console.log("receive the name, emitted to the backend:", name);
         setTimeout(() => {
             socket.emit("newUser", name);
-          }, 1000); // wait for 1 second before emitting the newUser event
+        }, 1000);
     }, [socket]);
 
     useEffect(() => {
         if (!socket)
             return
-        // test connection
         socket.on('userConnected', (received) => {
             console.log("user connected", received)
             setName(received);
@@ -43,38 +49,32 @@ function CodeEditor() {
         });
     }, [socket]);
 
-
-
-
     return (
         <div>
             <div>
-            {
-                userList.map((name) => <div> {name} </div>)
-            }
+                {
+                    userList.map((name) => <div> {name} </div>)
+                }
+            </div>
+            <div>
+                {language}
             </div>
             <Editor
                 height="90vh"
                 width="80vh"
-                defaultLanguage="javascript"
+                defaultLanguage={"javascript"}
                 defaultValue="// some comment"
             />
-            <Dropdown>
-                <Dropdown.Toggle variant="success">
-                    Open Menu
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    <Dropdown.Item href="#">
-                        Home Page
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#">
-                        Settings
-                    </Dropdown.Item>
-                    <Dropdown.Item href="#">
-                        Logout
-                    </Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+            <DropdownButton
+                alignRight
+                title="Dropdown right"
+                id="dropdown-menu-align-right"
+                onSelect={handleSelect}
+            >
+                <Dropdown.Item eventKey="python">Python</Dropdown.Item>
+                <Dropdown.Item eventKey="option-2">option-2</Dropdown.Item>
+                <Dropdown.Item eventKey="option-3">option 3</Dropdown.Item>
+            </DropdownButton>
         </div>
     )
 }
