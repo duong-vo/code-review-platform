5ee44a851ea3ff7b6968719a503de4ce58af966e
duong-vo <vodh2@miamioh.edu>
2023-04-20 23:42:07 -0400

    tried to center a div but did not succeed

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -8,7 +8,7 @@ import Form from 'react-bootstrap/Form';
 function Home() {
     const [room, setRoom] = useState("");
     const navigate = useNavigate();
-    
+
     const createRoom = () => {
         const roomId = Math.floor(Math.random() * 1000000).toString();
         // Redirect the user to the new room URL
@@ -19,17 +19,17 @@ function Home() {
         if (room !== "") {
             console.log(room);
             navigate(`/${room}`);
-          }
+        }
     }
 
     return (
         <div class="buttons-container">
-            <button type="submit" class="btn btn-dark" onClick={createRoom}>New Room</button>
-            <Form.Control 
-                          type="text" 
-                          placeholder="Enter room Id"
-                          onChange={(event) => { setRoom(event.target.value); }} />
-            <button class="btn btn-dark" onClick={joinRoom}>Join Room</button>
+                <button type="submit" class="btn btn-dark" onClick={createRoom}>New Room</button>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter room Id"
+                    onChange={(event) => { setRoom(event.target.value); }} />
+                <button class="btn btn-dark" onClick={joinRoom}>Join Room</button>
         </div>
     );
 }
